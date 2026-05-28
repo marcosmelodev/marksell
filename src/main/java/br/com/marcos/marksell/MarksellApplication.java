3c629391599eac42b3ee0f77e805f9de12876a62
Marcos Melo <marcos.dhl@hotmail.com>
2026-05-28 01:05:42 -0300

    Projeto criado

---
+++ src/main/java/br/com/marcos/marksell/MarksellApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos.marksell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarksellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarksellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcos/marksell/MarksellApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.marksell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarksellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
