56ffd0931fc0ac6ad5845576076723adefc0a71c
Marcos Melo <marcos.dhl@hotmail.com>
2026-05-28 08:55:52 -0300

    Service layer, component registration

---
+++ src/main/java/br/com/marcos/marksell/services/UserService.java
@@ -0,0 +1,25 @@
+package br.com.marcos.marksell.services;
+
+import br.com.marcos.marksell.entities.User;
+import br.com.marcos.marksell.repositories.UserResporitory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserResporitory resporitory;
+
+    public List<User> findAll(){
+        return resporitory.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = resporitory.findById(id);
+        return obj.get();
+    }
+}
