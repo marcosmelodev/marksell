0bf2ce034b421d0776e897595e2780b0b42906a9
Marcos Melo <marcos.dhl@hotmail.com>
2026-06-01 08:36:42 -0300

    Payment, one-to-one association

--- src/main/java/br/com/marcos/marksell/config/TestConfig.java
+++ src/main/java/br/com/marcos/marksell/config/TestConfig.java
@@ -71,5 +71,9 @@ public class TestConfig implements CommandLineRunner {
         p5.getCategories().add(cat2);
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 }
